509241ce825908dd3ae7986f541ed63e2c9e437d
bbbbb6969 <wkes12345678@gmail.com>
2022-04-24 22:04:03 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+bot=commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print("Bot is online")
+@bot.command()    #傳送某網路圖片
+async def 打我巴掌(ctx):
+    await ctx.send('http://n.sinaimg.cn/sinacn/w572h290/20180124/144f-fyqwiqk3525608.jpg')
+@bot.command()    #傳送某網路圖片
+async def 早安(ctx):
+    await ctx.send('https://truth.bahamut.com.tw/s01/202204/0e9a5503e5e827eee40da49963cba2bb.JPG')
+bot.run('OTY3NzcxNjgyNjM2OTcyMDYy.YmVJnQ.L9WNesvbLt0n-u1DCp8KN3V-scA')
