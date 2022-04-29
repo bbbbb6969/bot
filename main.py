9d98654bdd478b645b412d869597a172a35c7d75
bbbbb6969 <wkes12345678@gmail.com>
2022-04-29 16:33:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-import discord
-from discord.ext import commands
+import random
+manu={'飯糰':30,'豆漿':15,'奶茶':20,'蛋餅':25,'漢堡':35,'1號餐':45}
+manu1=['飯糰','豆漿','奶茶','蛋餅','漢堡','1號餐']
+manu2=[]
+list=[]
+for a in manu:
+    print(a,manu[a],'元')
+print('1號餐:漢堡+豆漿')
 
-bot=commands.Bot(command_prefix='!')
-
-@bot.event
-async def on_ready():
-    print("Bot is online")
-@bot.command()    #傳送某網路圖片
-async def 打我巴掌(ctx):
-    await ctx.send('http://n.sinaimg.cn/sinacn/w572h290/20180124/144f-fyqwiqk3525608.jpg')
-@bot.command()    #傳送某網路圖片
-async def 早安(ctx):
-    await ctx.send('https://truth.bahamut.com.tw/s01/202204/0e9a5503e5e827eee40da49963cba2bb.JPG')
-bot.run('OTY3NzcxNjgyNjM2OTcyMDYy.YmVJnQ.J1vT_7KGTIxN1SeMC82KqmCP-Do')
+while (True):
+    i=input('請輸入你要的餐點: ')
+    manu2.append(i)
+    if i=='不知道吃啥':
+        j=random.choice(manu1)
+        print('我推薦你吃',j,manu[j],'元')
+        list.append(manu[j])
+        break
+    else:
+        print(manu[i],'元')
+        k=input('是否繼續點餐?')
+        if k=='y':
+            continue
+        elif k=='n':
+            for c in manu2:
+                list.append(manu[c])
+            print('一共是',sum(list),'元')
+            break
+        else:
+            print('從頭再一次')
+            list.clear()
+            continue
+money=int(input('請輸入你給的錢: '))
+total=sum(list)
+if money<total:
+    print('錢不夠')
+else:
+    print('找你',money-total,'元')
